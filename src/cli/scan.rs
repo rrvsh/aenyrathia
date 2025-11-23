8a9285786f3c1ca24bacc22c555282f35323200b
Mohammad Rafiq <rafiq@rrv.sh>
2025-11-23 20:27:45 +0800

    feat: pretty print file names and paths

--- src/cli/scan.rs
+++ src/cli/scan.rs
@@ -1,5 +1,7 @@
 use clap::Args;
 use ignore::Walk;
+use std::path::Path;
+use titlecase::titlecase;
 
 #[derive(Args)]
 pub struct ScanArgs {
@@ -9,8 +11,26 @@ pub struct ScanArgs {
 
 impl ScanArgs {
     pub fn run(&self) {
-        for entry in Walk::new(&self.path) {
-            println!("{}", entry.unwrap().path().display());
+        for entry in Walk::new(&self.path)
+            .flatten()
+            .filter(|x| !x.path().is_dir())
+        {
+            let mut components = entry.path().components();
+            components.next();
+            let pretty_components = components
+                .map(|x| {
+                    titlecase(
+                        &Path::new(x.as_os_str())
+                            .file_stem()
+                            .expect("No file name found.")
+                            .to_str()
+                            .expect("Not valid Unicode.")
+                            .replace('-', " "),
+                    )
+                })
+                .collect::<Vec<_>>()
+                .join("/");
+            println!("{pretty_components:?}");
         }
     }
 }
