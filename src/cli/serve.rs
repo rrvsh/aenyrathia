86e2f3246fa317f8ee3d16fbb374b4ce6ddae03e
rafiq <rafiq@rrv.sh>
2025-12-17 18:02:46 +0800

    chore: remove hello world route (#19)

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,7 +1,4 @@
-use askama::Template;
 use axum::Router;
-use axum::http::StatusCode;
-use axum::routing::get;
 use clap::Args;
 
 #[derive(Args)]
@@ -11,21 +8,6 @@ impl ServeArgs {
     #[tokio::main]
     pub async fn run() {
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-        axum::serve(listener, Router::new().route("/hello", get(hello)))
-            .await
-            .unwrap();
+        axum::serve(listener, Router::new()).await.unwrap();
     }
-}
-
-#[derive(Template)]
-#[template(path = "hello.html")]
-struct HelloTemplate<'a> {
-    name: &'a str,
-}
-
-async fn hello() -> Result<String, StatusCode> {
-    let template = HelloTemplate { name: "world" };
-    template
-        .render()
-        .map_or_else(|_| Err(StatusCode::INTERNAL_SERVER_ERROR), Ok)
 }
