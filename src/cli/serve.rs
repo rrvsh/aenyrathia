554b86d7c6a5be20267a93862e066e4f0e413ab6
Mohammad Rafiq <rafiq@rrv.sh>
2025-12-17 18:59:40 +0800

    feat: add get route for /wiki/<path>

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,6 +1,14 @@
+use askama::Template;
 use axum::Router;
+use axum::extract::Path;
+use axum::http::StatusCode;
+use axum::response::Html;
+use axum::routing::get;
 use clap::Args;
 use log::info;
+use log::warn;
+use markdown::to_html;
+use std::fs;
 
 #[derive(Args)]
 pub struct ServeArgs {}
@@ -10,6 +18,42 @@ impl ServeArgs {
     pub async fn run() {
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
         info!("Starting axum router and listening on 0.0.0.0:3000");
-        axum::serve(listener, Router::new()).await.unwrap();
+        axum::serve(
+            listener,
+            Router::new().route("/wiki/{*article_path}", get(wiki_page)),
+        )
+        .await
+        .unwrap();
     }
+}
+
+#[derive(Template)]
+#[template(path = "article.html")]
+struct WikiArticleTemplate {
+    content: String,
+}
+
+async fn wiki_page(Path(article_path): Path<String>) -> Result<Html<String>, StatusCode> {
+    // note: article_path resolves without preceding slash
+    let wiki_directory = "wiki/".to_string();
+    let file_path = wiki_directory + &article_path + ".md";
+    fs::read_to_string(&file_path).map_or_else(
+        |e| {
+            warn!("Couldn't read {file_path} to string: {e}");
+            Err(StatusCode::NOT_FOUND)
+        },
+        |file_content| {
+            WikiArticleTemplate {
+                content: to_html(&file_content),
+            }
+            .render()
+            .map_or_else(
+                |e| {
+                    warn!("Error rendering template for {file_path}: {e}");
+                    Err(StatusCode::INTERNAL_SERVER_ERROR)
+                },
+                |rendered| Ok(Html(rendered)),
+            )
+        },
+    )
 }
