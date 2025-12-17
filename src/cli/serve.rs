c87857fa8e932217b0724c241d6f150e64279763
Mohammad Rafiq <rafiq@rrv.sh>
2025-12-17 13:05:50 +0800

    strip out changes

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,40 +1,20 @@
 use axum::Router;
-use axum::extract::Path;
-use axum::http::StatusCode;
 use axum::routing::get;
 use clap::Args;
-use pinbreak::domain::markdown::Markdown;
 
 #[derive(Args)]
 pub struct ServeArgs {}
 
 impl ServeArgs {
-    pub fn run() {
-        serve();
+    #[tokio::main]
+    pub async fn run() {
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+        axum::serve(listener, Router::new().route("/hello", get(hello)))
+            .await
+            .unwrap();
     }
-}
-
-#[tokio::main]
-async fn serve() {
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-
-    axum::serve(
-        listener,
-        Router::new()
-            .route("/hello", get(hello))
-            .route("/wiki/{*article_path}", get(wiki_page)),
-    )
-    .await
-    .unwrap();
 }
 
 async fn hello() -> String {
     "Hello, World!".to_string()
-}
-
-async fn wiki_page(Path(article_path): Path<String>) -> Result<String, StatusCode> {
-    // note: article_path resolves without preceding slash
-    let wiki_directory = "wiki/".to_string();
-    Markdown::from_path(wiki_directory + &article_path + ".md")
-        .map_or_else(|_| Err(StatusCode::NOT_FOUND), |md| Ok(md.as_html()))
 }
