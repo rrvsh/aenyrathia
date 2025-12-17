4e6f6dcbe92f7f9778c23c4c131ad907d31e8041
rafiq <rafiq@rrv.sh>
2025-12-17 18:52:00 +0800

    build: add logging via `env_logger` (#20)
    * build: add env_logger and log
    
    * feat: add logging for serve

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use env_logger::Builder;
+use env_logger::Env;
 use std::io;
+
 mod cli;
 
 fn main() -> io::Result<()> {
+    let env = Env::new().filter("PB_LOG");
+    let mut builder = Builder::from_env(env);
+    builder.init();
+
     cli::run()
 }
