10c4db44a55de83a02a65b1525ebac60dae078ee
rafiq <rafiq@rrv.sh>
2025-12-17 18:52:00 +0800

    build: add logging via `env_logger` (#20)
    * build: add env_logger and log
    
    * feat: add logging for serve

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,5 +1,6 @@
 use axum::Router;
 use clap::Args;
+use log::info;
 
 #[derive(Args)]
 pub struct ServeArgs {}
@@ -8,6 +9,7 @@ impl ServeArgs {
     #[tokio::main]
     pub async fn run() {
         let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+        info!("Starting axum router and listening on 0.0.0.0:3000");
         axum::serve(listener, Router::new()).await.unwrap();
     }
 }
