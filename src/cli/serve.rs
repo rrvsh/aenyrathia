8a80be4debc65f6714a95c601c294e269f078cc4
rafiq <rafiq@rrv.sh>
2025-12-22 10:58:22 +0800

    feat(wiki): add `GET` and `POST` routes for `/edit/wiki/<path>` (#22)
    * feat: add get route with form for /edit/wiki/<path>
    
    * fix: make textarea preserve newlines
    
    * enhancement: use proper log levels
    
    * feat: implement file path validation for `GET /edit/wiki`
    
    * feat: add `POST /edit/wiki/<path>` route with redirect
    
    * build: add serde
    
    * feat: add updating markdown with `POST /edit/wiki/<path>`

--- src/cli/serve.rs
+++ src/cli/serve.rs
@@ -1,14 +1,15 @@
 use askama::Template;
 use axum::Router;
-use axum::extract::Path;
+use axum::extract::{Form, Path};
 use axum::http::StatusCode;
-use axum::response::Html;
+use axum::response::{Html, Redirect};
 use axum::routing::get;
 use clap::Args;
-use log::info;
-use log::warn;
+use log::{debug, error, info, trace, warn};
 use markdown::to_html;
+use serde::Deserialize;
 use std::fs;
+use std::path::{self, PathBuf};
 
 #[derive(Args)]
 pub struct ServeArgs {}
@@ -20,7 +21,9 @@ impl ServeArgs {
         info!("Starting axum router and listening on 0.0.0.0:3000");
         axum::serve(
             listener,
-            Router::new().route("/wiki/{*article_path}", get(wiki_page)),
+            Router::new()
+                .route("/wiki/{*article_path}", get(wiki_page))
+                .route("/edit/wiki/{*article_path}", get(edit_get).post(edit_post)),
         )
         .await
         .unwrap();
@@ -54,6 +57,83 @@ async fn wiki_page(Path(article_path): Path<String>) -> Result<Html<String>, Sta
                 },
                 |rendered| Ok(Html(rendered)),
             )
+        },
+    )
+}
+
+#[derive(Template)]
+#[template(path = "editor.html")]
+struct EditorTemplate {
+    file_content: String,
+}
+
+async fn edit_get(Path(article_path): Path<String>) -> Result<Html<String>, StatusCode> {
+    // note: article_path resolves without preceding slash
+    let path = PathBuf::from("wiki")
+        .join(&article_path)
+        .with_extension("md");
+    trace!("Rendering template for /edit/{}.", path.display());
+    if !path_editable(&path) {
+        debug!("{} not editable.", path.display());
+        return Err(StatusCode::NOT_FOUND);
+    }
+    fs::read_to_string(&path).map_or_else(
+        |e| {
+            warn!("Couldn't read {} to string: {}", path.display(), e);
+            Err(StatusCode::NOT_FOUND)
+        },
+        |file_content| {
+            trace!("file_content for {}: {}", path.display(), file_content);
+            EditorTemplate { file_content }.render().map_or_else(
+                |e| {
+                    error!("Error rendering template for {}: {}", path.display(), e);
+                    Err(StatusCode::INTERNAL_SERVER_ERROR)
+                },
+                |rendered| {
+                    trace!("rendered html for /edit/wiki/{article_path}: {rendered}");
+                    Ok(Html(rendered))
+                },
+            )
+        },
+    )
+}
+
+/// Checks if a file path is editable by checking if any of the following conditions are true:
+/// 1. The file already exists and is not a directory
+/// 2. The parent directory exists
+fn path_editable<P: AsRef<path::Path>>(path: P) -> bool {
+    let path = path.as_ref();
+    path.is_file() || path.parent().is_some_and(path::Path::is_dir)
+}
+
+#[derive(Deserialize)]
+struct EditForm {
+    markdown: String,
+}
+
+fn normalise_newlines(input: &str) -> String {
+    input.replace("\r\n", "\n").replace('\r', "\n")
+}
+
+async fn edit_post(
+    Path(article_path): Path<String>,
+    Form(form): Form<EditForm>,
+) -> Result<Redirect, StatusCode> {
+    let path = PathBuf::from("wiki")
+        .join(&article_path)
+        .with_extension("md");
+    if !path_editable(&path) {
+        debug!("{} not editable.", path.display());
+        return Err(StatusCode::NOT_FOUND);
+    }
+    fs::write(&path, normalise_newlines(&form.markdown)).map_or_else(
+        |e| {
+            warn!("Couldn't write to file {}: {}", path.display(), e);
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        },
+        |()| {
+            let wiki_path = "/wiki/".to_owned() + &article_path;
+            Ok(Redirect::to(&wiki_path))
         },
     )
 }
