e3ef39955fadc07a473c78eb4175497c842b6d1b
rafiq <rafiq@rrv.sh>
2025-12-29 10:37:36 +0800

    feat(git): run fetch and push on a separate thread (#43)
    * minor formatting
    
    * feat(git): run fetch and push on a separate thread

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,5 +1,6 @@
 use super::settings::AppSettings;
 use crate::git::GitRemote;
+use std::time::Duration;
 
 #[derive(Clone)]
 pub struct AppState {
@@ -9,7 +10,10 @@ pub struct AppState {
 impl AppState {
     pub fn init(settings: &AppSettings) -> Self {
         Self {
-            remote: GitRemote::init(&settings.git_remote),
+            remote: GitRemote::init_with_interval(
+                &settings.git_remote,
+                Duration::from_millis(settings.git_sync_interval_ms),
+            ),
         }
     }
 }
