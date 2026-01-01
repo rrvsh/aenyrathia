44506bcdc76fb2568f713d291933fe1795185538
rafiq <rafiq@rrv.sh>
2026-01-01 20:28:28 +0800

    hotfix: use unix socket for connection (#58)

--- src/app/settings.rs
+++ src/app/settings.rs
@@ -2,7 +2,7 @@ pub struct AppSettings {
     pub addr: String,
     pub git_remote: String,
     pub git_sync_interval_ms: u64,
-    pub db_url: String,
+    pub db_options: sqlx::postgres::PgConnectOptions,
 }
 
 impl AppSettings {
@@ -16,15 +16,28 @@ impl AppSettings {
             .and_then(|v| v.parse::<u64>().ok())
             .filter(|v| *v > 0)
             .unwrap_or(1000);
-        let db_url = std::env::var("DATABASE_URL").unwrap_or_else(|_| {
-            "postgres://username:password@localhost:54322/default_database".to_string()
+        let db_host = std::env::var("PGHOST").unwrap_or_else(|_| {
+            "/cloudsql/aenyrathia:asia-southeast1:aenyrathia-pgsql".to_string()
         });
+        let db_port = std::env::var("PGPORT")
+            .ok()
+            .and_then(|v| v.parse::<u16>().ok())
+            .unwrap_or(5432);
+        let db_user = std::env::var("PGUSER").unwrap_or_else(|_| "rafiq".to_string());
+        let db_password = std::env::var("PGPASSWORD").unwrap_or_else(|_| "1234".to_string());
+        let db_name = std::env::var("PGDATABASE").unwrap_or_else(|_| "app_db".to_string());
+        let db_options = sqlx::postgres::PgConnectOptions::new()
+            .host(&db_host)
+            .port(db_port)
+            .username(&db_user)
+            .password(&db_password)
+            .database(&db_name);
 
         Self {
             git_remote,
             addr: format!("{host}:{port}"),
             git_sync_interval_ms,
-            db_url,
+            db_options,
         }
     }
 }
