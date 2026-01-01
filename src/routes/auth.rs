664b5f0cef7049c65aa953ea39b8d1e53e3e5cb7
rafiq <rafiq@rrv.sh>
2026-01-01 11:47:46 +0800

    feat: add registration flow (#46)
    Added `/register` GET and POST.
    Added `user_data` table.

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -1,8 +1,18 @@
-use axum::Router;
-use axum::extract::Form;
-use axum::response::Redirect;
-use axum::routing::post;
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHasher, SaltString, rand_core::OsRng},
+};
+use askama::Template;
+use axum::{
+    Extension, Router,
+    extract::Form,
+    http::StatusCode,
+    response::{Html, Redirect},
+    routing::{get, post},
+};
+use log::error;
 use serde::Deserialize;
+use sqlx::PgPool;
 use tower_cookies::{Cookie, Cookies};
 
 pub struct AuthRouter {}
@@ -12,6 +22,7 @@ impl AuthRouter {
         Router::new()
             .route("/login", post(login))
             .route("/logout", post(logout))
+            .route("/register", get(register_get).post(register_post))
     }
 }
 
@@ -28,4 +39,53 @@ pub async fn login(cookies: Cookies, Form(form): Form<LoginRequest>) -> Redirect
 pub async fn logout(cookies: Cookies) -> Redirect {
     cookies.remove(Cookie::new("username", ""));
     Redirect::to("/")
+}
+
+#[derive(Template)]
+#[template(path = "register.html")]
+struct RegisterTemplate {}
+
+pub async fn register_get() -> Result<Html<String>, StatusCode> {
+    RegisterTemplate {}.render().map_or_else(
+        |e| {
+            error!("Error rendering register template: {e}");
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        },
+        |rendered| Ok(Html(rendered)),
+    )
+}
+
+#[derive(Deserialize)]
+pub struct RegisterForm {
+    fullname: String,
+    email: String,
+    password: String,
+}
+
+pub async fn register_post(
+    db: Extension<PgPool>,
+    Form(form): Form<RegisterForm>,
+) -> Result<Redirect, StatusCode> {
+    let RegisterForm {
+        fullname,
+        email,
+        password,
+    } = form;
+
+    let salt = SaltString::generate(&mut OsRng);
+    let password_hash = Argon2::default()
+        .hash_password(password.as_bytes(), &salt)
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    sqlx::query!(
+        "insert into user_data (full_name, email, password_hash) values ($1, $2, $3)",
+        fullname,
+        email,
+        password_hash.to_string(),
+    )
+    .execute(&*db)
+    .await
+    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Redirect::to("/"))
 }
